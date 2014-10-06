7df33adcc12d62dd3d1200975113722a7d37e453
evx001 <eviexeris@gmail.com>
2014-10-06 02:04:16 -0400

    Outlets and Actions

--- TipCalc/ViewController.swift
+++ TipCalc/ViewController.swift
@@ -9,12 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var totalTextField: UITextField!
+    @IBOutlet weak var taxPctSlider: UISlider!
+    @IBOutlet weak var taxPctLabel: UILabel!
+    @IBOutlet weak var resultsTextView: UITextView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    @IBAction func calculateTapped(sender: AnyObject) {
+    }
 
+    @IBAction func taxPercentChaged(sender: AnyObject) {
+    }
+    @IBAction func viewTapped(sender: AnyObject) {
+        
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
